1debbe701dcb011bcc860102b96d3a06c7638a1d
Elaz <elaz@rounds.com>
2019-02-25 13:53:12 +0200

    0.8.0

--- EcosystemSampleApp/AppDelegate.swift
+++ EcosystemSampleApp/AppDelegate.swift
@@ -45,6 +45,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        if Kin.shared.canHandleURL(url) {
+            Kin.shared.handleURL(url, options: options)
+            return true
+        }
+        
+        return false
+    }
 }
 
--- Podfile
+++ Podfile
@@ -2,7 +2,7 @@
 target 'EcosystemSampleApp' do
   use_frameworks!
 
-  pod 'KinEcosystem', '0.6.3'
+  pod 'KinEcosystem', :path => '../kin-ecosystem-ios-sdk/KinEcosystem.podspec'
   pod 'JWT', '3.0.0-beta.11', :modular_headers => true
   pod 'HockeySDK', :modular_headers => true
 
